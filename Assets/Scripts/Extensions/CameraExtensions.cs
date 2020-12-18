0829dcb227a78664f9082faa859f458644a29367
Fabian Schober <fabian@dirtypaws.studio>
2020-12-18 19:11:11 +0100

    first commit

---
+++ Assets/Scripts/Extensions/CameraExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Extensions
+{
+    public static class CameraExtensions
+    {
+        public static Bounds OrthographicBounds(this Camera camera)
+        {
+            var height = camera.orthographicSize * 2;
+            var width = height * camera.aspect;
+
+            Bounds bounds = new Bounds(
+                camera.transform.position,
+                new Vector3(width, height, 0));
+
+            return bounds;
+        }
+    }
+}
---
+++ Assets/Scripts/Extensions/LayerExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Extensions
+{
+    public static class LayerExtensions
+    {
+        public static bool Test(this LayerMask mask, int index)
+        {
+            return (mask & (1 << index)) != 0;
+        }
+
+        public static bool Test(this LayerMask mask, GameObject obj)
+        {
+            return Test(mask, obj.layer);
+        }
+
+        public static bool Test(this LayerMask mask, Component comp)
+        {
+            return Test(mask, comp.gameObject.layer);
+        }
+
+        public static int[] GetSortingLayerMask(params string[] layerNames)
+        {
+            return layerNames.Select(SortingLayer.NameToID).ToArray();
+        }
+    }
+}
