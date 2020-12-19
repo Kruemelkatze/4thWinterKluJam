e1c81f47d2a9207b3039ea437c214e9cb7258dbd
Kruemelkatze <schoberf@gmx.net>
2020-12-19 19:22:57 +0100

    fixed pickups

--- Assets/Scripts/Cards/PickupCard.cs
+++ Assets/Scripts/Cards/PickupCard.cs
@@ -8,9 +8,9 @@ namespace Cards
         {
             var playerStats = GameController.Instance.playerCard.stats;
 
-            playerStats.attack = Mathf.Max(1, playerStats.health + stats.attack);
+            playerStats.attack = Mathf.Max(1, playerStats.attack + stats.attack);
             playerStats.health = Mathf.Max(0, playerStats.health + stats.health);
-            playerStats.armor = Mathf.Max(0, playerStats.health + stats.armor);
+            playerStats.armor = Mathf.Max(0, playerStats.armor + stats.armor);
 
             GameController.Instance.playerCard.UpdateStats(playerStats);
 
