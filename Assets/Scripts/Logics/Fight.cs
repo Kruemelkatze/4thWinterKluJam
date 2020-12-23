c209d761ae930cd0ec98f175af7f78ff23405aff
Guido <guidopr@edu.aau.at>
2020-12-23 15:13:36 +0100

    New fight logic, inital tweaks of cards, updated tutorial

--- Assets/Scripts/Logics/Fight.cs
+++ Assets/Scripts/Logics/Fight.cs
@@ -18,20 +18,30 @@ namespace Logics
             statsEnemy.health = Mathf.Max(0, statsEnemy.health - damageToEnemy);
 
             // Armor
-            statsEnemy.armor = Mathf.Max(0, statsEnemy.armor - 1);
+            statsEnemy.tempArmor = statsEnemy.armor; //sets armor value as tempArmor
+            statsEnemy.armor = Mathf.Max(0, statsEnemy.armor - statsPlayer.attack);
 
             // Attack
-            statsPlayer.attack = Mathf.Max(1, statsPlayer.attack - 1);
+            if (statsEnemy.armor != statsEnemy.tempArmor) //compares armor with tempArmor
+            {
+                statsPlayer.attack = Mathf.Max(1, statsPlayer.attack - 1); //reduces attack if mosnter has armour
+            }
+
 
             // Enemy Attacks if not dead
             if (statsEnemy.health <= 0)
                 return (statsPlayer, statsEnemy, FightResult.PlayerWon);
 
             // Enemy Attacks
+            statsPlayer.tempArmor = statsPlayer.armor; //sets armor value as tempArmor
             var damageToPlayer = Mathf.Max(0, statsEnemy.attack - statsPlayer.armor);
             statsPlayer.health = Mathf.Max(0, statsPlayer.health - damageToPlayer);
-            statsPlayer.armor = Mathf.Max(0, statsPlayer.armor - 1);
-            //statsEnemy.attack = Mathf.Max(1, statsEnemy.attack - 1);
+            statsPlayer.armor = Mathf.Max(0, statsPlayer.armor - statsEnemy.attack);
+
+            if (statsPlayer.armor != statsPlayer.tempArmor) // compares armor with tempArmor
+            {
+                statsEnemy.attack = Mathf.Max(0, statsEnemy.attack - 1); // reduces attack if player has armour
+            }
 
             var result = statsPlayer.health <= 0 ? FightResult.EnemyWon : FightResult.Draw;
             return (statsPlayer, statsEnemy, result);
--- Assets/Scripts/Logics/Stats.cs
+++ Assets/Scripts/Logics/Stats.cs
@@ -8,19 +8,22 @@ namespace Logics
         public int attack;
         public int armor;
         public int health;
+        public int tempArmor; //temp armor added
 
         public Stats(Stats baseStats)
         {
             attack = baseStats.attack;
             armor = baseStats.armor;
             health = baseStats.health;
+            tempArmor = baseStats.tempArmor; //temp armor added
         }
 
-        public Stats(int attack, int armor, int health)
+        public Stats(int attack, int armor, int health, int tempArmor)
         {
             this.attack = attack;
             this.armor = armor;
             this.health = health;
+            this.tempArmor = tempArmor; //temp added
         }
     }
 }
